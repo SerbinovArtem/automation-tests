4f828af59b8f71d866fcaa22aa0d9f0ff2ea6f81
SerbinovArtem <serbinoffsky@gmail.com>
2026-09-20 14:54:55 +0300

    Add login test

---
+++ login-test.js
@@ -0,0 +1,10 @@
+const username = "testuser";
+const password = "123456";
+
+console.log("Testing login...");
+
+if (username && password) {
+    console.log("Login test passed");
+} else {
+    console.log("Login test failed");
+}
